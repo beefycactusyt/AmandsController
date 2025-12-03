1f380611d3b25eece33a8990f28ea4b35a935e97
Beefy <67741788+beefycactusyt@users.noreply.github.com>
2025-12-03 23:28:17 +0000

    runs, ingame config shows, mod doesnt work.
    removed SSAA for now. cant get it to work. probably broke magnetism with it too.
    changed necessary dependencies for 4.0

---
+++ AmandsController400/TaskExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AmandsControllerMod
+{
+    public static class TaskExtensions
+    {
+        public static void HandleExceptions(this Task task)
+        {
+            task.ContinueWith(t =>
+            {
+                if (t.Exception != null)
+                {
+                    UnityEngine.Debug.LogException(t.Exception);
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        public static void HandleExceptions<T>(this Task<T> task)
+        {
+            task.ContinueWith(t =>
+            {
+                if (t.Exception != null)
+                {
+                    UnityEngine.Debug.LogException(t.Exception);
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
